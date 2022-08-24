ad943145f3ea423977ce4d7b5781b5acfaad7b3e
IgnacioIraola <iraolai@hotmail.com>
2022-08-23 22:01:03 -0300

    Se agrega total

--- app.js
+++ app.js
@@ -5,19 +5,38 @@ class Producto{
     }
 }
 
+
+//Ingreso Compra
 const compras = []
 
-for (let i = 0; i<=1; i++) {
+for (let i = 0; i<=2; i++) {
     let bebida = prompt("Ingrese la bebida deseada")
-    let precio = prompt("Ingrese la bebida deseada")
+    let precio = prompt("Ingrese el precio")
 
     let producto = new Producto(bebida,precio)
    compras.push(producto);
 }
+
+//DOM
 let ul = document.createElement("ul");
 let inner = '';
 for (const compra of compras) {
-    inner+= `<li>${compra}</li>`;
+    inner+= `<li>${compra.bebida} por un valor de $ ${compra.precio}</li>`;
 }
 ul.innerHTML = inner;
 document.body.appendChild(ul);
+
+
+
+//calculo total
+const precioBebidas = compras.map(dato => dato.precio)
+let precioTotales = precioBebidas.reduce((a,b) =>a+b)
+
+
+let tot = document.createElement("h5");
+let importetotal= '';
+for (const precioTotal of precioTotales) {
+    importetotal+= `<h5> El total es de: $${precioTotal}</h5>`;
+}
+tot.innerHTML = importetotal;
+document.body.appendChild(tot);
